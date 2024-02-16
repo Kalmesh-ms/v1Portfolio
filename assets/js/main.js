382fea90f41cd0db6810e75f77ccb903e53de7f0
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2024-02-16 17:10:35 +0530

    #work #testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,12 +33,36 @@ modalClose.forEach((mc) => {
 
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortfolio = mixitup('.work__container', {
+    selectors : {
+        target : '.work__card'
+    },
+    animation : {
+        duration : 300
+    }
+});
 
 /* Link active work */ 
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+    linkWork.forEach(l => l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l => l.addEventListener('click', activeWork))
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor:true,
+    pagination: {
+        el : ".swiper-pagination",
+        clickable: true,
+    },
+}) 
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
